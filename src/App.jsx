72f03b46d375545ecf30813be50bb9e1ba70a9b2
@chea99 <chea02310@gmail.com>
2025-05-20 07:22:13 +0700

    update on hero section putting 3D room into it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Hero from "./sections/Hero"
 
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1>Hello, my name is Vichea</h1>
-        </div>
-    </main>
+    <>
+        <Hero />
+    </>
   )
 }
 
---
+++ src/components/HeroModels/HeroExperience.jsx
@@ -0,0 +1,34 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'
+import { useMediaQuery } from 'react-responsive';
+import { Room } from './Room';
+import HeroLight from './HeroLight';
+import Particles from './Particles';
+const HeroExperience = () => {
+  const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
+  const isMobile = useMediaQuery({query: '(max-width: 768px)'});
+  return (
+    <Canvas camera={{position: [0,0,15], fov:45}}>
+     
+      <OrbitControls 
+      enablePan={false} 
+      enableZoom={!isTablet}
+      minDistance={5}
+      maxDistance={20}
+      minPolarAngle={Math.PI / 5}
+      maxPolarAngle={Math.PI / 2}
+      />
+      <HeroLight />
+      <Particles count={100}/>
+      <group scale={isMobile ? 0.7 : 1} 
+      position={isMobile ? [0, -3.5, 0] : isTablet ? [0, -2, 0] : [0, -3, 0]}
+      rotation={[0, -Math.PI / 3, 0]}
+      >
+         <Room/>
+      </group>
+     
+    </Canvas>
+  )
+}
+
+export default HeroExperience;
